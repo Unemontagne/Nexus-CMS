e6fbb9d9f531efd64e036e6dac45a243cf43ace9
I-is-as-I-does <66915011+I-is-as-I-does@users.noreply.github.com>
2022-03-23 17:50:12 +0100

    update

--- _src/assets/js/nx-data.11ty.js
+++ _src/assets/js/nx-data.11ty.js
@@ -1,62 +1,73 @@
 class NxData {
-    categoryInfos(data, catg){
-      var nf = {
-        list: [],
-        deflt: ''
-      }
-      if(data[catg]){
-        
-        nf.list = Object.values(data[catg]).map(it => it.meta_filename)
-        var index = 0
-        var k = 'default_'+catg.slice(0,-1)
+  categoryInfos(data, catg){
+    var nf = {
+      list: [],
+      deflt: ''
+    }
+    if(data[catg]){
+      
+      nf.list = Object.values(data[catg]).map(it => it.meta_filename)        
 
+      if(nf.list.length){  
+        var index = 0;
+        var defname = ''   
+        var k = 'default_'+catg.slice(0,-1)  
         if(data.settings['_'+catg][k]){
           var searchindex = nf.list.indexOf(data.settings['_'+catg][k])
           if(searchindex !== -1) {
             index = searchindex       
-          }
+          } 
         }
+        defname = nf.list[index]
         if(catg === "nexus"){
-          nf.deflt = data.settings._site.url+"/source/"+nf.list[index]+".json"
-          if(data.settings._nexus.load_first_thread && data.nexus[nf.list[index]].threads && data.nexus[nf.list[index]].threads.length){
-            nf.deflt += "#" + data.nexus[nf.list[index]].threads[0].id
+          nf.deflt = data.settings._site.url+"/source/"+defname+".json"
+          if(data.settings._nexus.load_first_thread){
+            var dt = null;
+            if(data.nexus[defname]){
+              dt = data.nexus[defname]
+            } else {
+              dt = Object.values(data.nexus)[index]
             }
+            if (dt && dt.threads && dt.threads.length){
+              nf.deflt += "#" + dt.threads[0].id
+              }
+          } 
         } else {
-          nf.deflt = data.settings._site.url+"/pages/"+nf.list[index]
-        }
-
+          nf.deflt = data.settings._site.url+"/pages/"+defname
+        }  
       }
-      return nf
     }
-    data() {
-      return {
-        permalink: "/assets/js/NxData.js"
-      };
-    }
-    render(data) {
-
-      var nexus = this.categoryInfos(data, "nexus")
-      var keys = ['script', 'lang', 'style'];
-      var fields = ['script_url', 'default_lang', 'custom_theme_url'];
-      for(var i=0; i<keys.length; i++){
-        nexus[keys[i]] = data.settings._nexus[fields[i]]
-      }
-      var pages = this.categoryInfos(data, "pages")
-      if(data.settings._pages.custom_theme_url){
-        pages.style = data.settings._pages.custom_theme_url
-      } else {
-        pages.style = '/assets/css/NxPages.css'
-      }
-      pages.back = data.settings._pages.display_home_link
-      var nxdata = {
-        site: {
-          url: data.settings._site.url
-        },
-        nexus: nexus,
-        pages: pages
-      }
-      return 'window.NxData = '+JSON.stringify(nxdata)
-    }
+    return nf
   }
-  
+  data() {
+    return {
+      permalink: "/assets/js/NxData.js"
+    };
+  }
+  render(data) {
+
+    var nexus = this.categoryInfos(data, "nexus")
+    var keys = ['script', 'lang', 'style'];
+    var fields = ['script_url', 'default_lang', 'custom_theme_url'];
+    for(var i=0; i<keys.length; i++){
+      nexus[keys[i]] = data.settings._nexus[fields[i]]
+    }
+    var pages = this.categoryInfos(data, "pages")
+    if(data.settings._pages.custom_theme_url){
+      pages.style = data.settings._pages.custom_theme_url
+    } else {
+      pages.style = '/assets/css/NxPages.css'
+    }
+    pages.back = data.settings._pages.display_home_link
+    var nxdata = {
+      site: {
+        url: data.settings._site.url
+      },
+      nexus: nexus,
+      pages: pages
+    }
+    return 'window.NxData = '+JSON.stringify(nxdata)
+  }
+}
+
 module.exports = NxData;
